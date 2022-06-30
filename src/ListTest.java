174e462f5c0bad5f53cd1841093a13284ccfaaef
fa1seW <74900409+fa1seW@users.noreply.github.com>
2022-06-30 23:57:24 +0800

    Collections

--- src/ListTest.java
+++ src/ListTest.java
@@ -1,7 +1,5 @@
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ListTest {
 
@@ -29,5 +27,27 @@ public class ListTest {
         for (Student st : students) {
             System.out.println("学生：" + st.id + ":" + st.name);
         }
+    }
+
+    public void testIterator() {
+        Iterator<Student> it = students.iterator();
+        while (it.hasNext()) {
+            Student st = it.next();
+            System.out.println("学生" + st.id + ":" + st.name);
+        }
+    }
+
+    public void testForEach() {
+        System.out.println("有如下学生（通过 for each）：");
+        for (Student obj : students) {
+            Student st = obj;
+            System.out.println("学生：" + st.id + ":" + st.name);
+        }
+        //使用 java8 Stream 将学生排序后输出
+        students.stream()//创建 Stream
+                //通过学生 id 排序
+                .sorted(Comparator.comparing(x -> x.id))
+                //输出
+                .forEach(System.out::println);
     }
 }
