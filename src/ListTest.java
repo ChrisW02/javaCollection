afc0082fa49bde227bace531be64aebbc2bdbe4d
fa1seW <74900409+fa1seW@users.noreply.github.com>
2022-06-30 23:54:16 +0800

    Collections

---
+++ src/ListTest.java
@@ -0,0 +1,36 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListTest {
+
+    public List<Student> students;
+
+    public ListTest() {
+        this.students = new ArrayList<Student>();
+    }
+
+    public void testAdd() {
+        Student st1 = new Student("1", "张三");
+        students.add(st1);
+
+        Student temp = students.get(0);
+        System.out.println(temp.id + ':' + temp.name);
+
+        Student st2 = new Student("2", "李四");
+        students.add(0, st2);
+
+        Student[] student = {new Student("3", "王五"), new Student("4", "马六")};
+        students.addAll(Arrays.asList(student));
+    }
+
+    public void testGet() {
+        for (Student st : students) {
+            System.out.println("学生：" + st.id + ":" + st.name);
+        }
+    }
+
+    public void testIterator
+
+}
